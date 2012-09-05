0dfe583cf533ab0816f4edb1815efb13fd54cd28
valachi <impulse221@yandex.ru>
2012-09-05 18:31:35 +0400

    initial commit with initial style

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'haml'
+gem 'compass-rails'
+
+gem 'sqlite3', group: :development
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Niceglass::Application.config.secret_token = '51c07a8850f2a77cd3a14376a092067fc7484fef1c1caed38f7e6911d4271eb8aaa6b2bb830b941b9d34403779c19319006043915a9c306a6413da4b9e0535db'
