4b8e664646870c2cb50cf5d7705d21c5608aa4e8
valachi <impulse221@yandex.ru>
2012-09-10 17:06:46 +0400

    Added routes, fix html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Niceglass::Application.routes.draw do
   root to: 'pages#index'
+  match 'gallery', to: 'pages#gallery', as: :gallery
+  match 'services', to: 'pages#services', as: :services
+  match 'contacts', to: 'pages#contacts', as: :contacts
 end
